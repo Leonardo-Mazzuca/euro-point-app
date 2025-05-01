02b776f22b13dfc1d5d7664395b55b7c8a5108f7
Leonardo Mazzuca <leonardodesouzamazzuca8@gmail.com>
2025-05-01 12:25:54 -0300

    adding animation to home

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,6 @@ import {
   Text,
   View,
 } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "@/components/header";
 import { Tabs, TabsList, TabsTrigger } from "@/components/Tabs";
 import { Input } from "@/components/Input";
@@ -13,32 +12,51 @@ import AntDesign from "@expo/vector-icons/AntDesign";
 import PostCard from "@/components/post-card";
 import { cn } from "@/lib/utils";
 import TabsContainer from "@/components/tabs-container";
+import { useLayoutContext } from "@/context/layout-context";
 
 const Home = () => {
   const [currentScreen, setCurrentScreen] = useState<HomeScreen>("for-you");
   const uiOpacity = useRef(new Animated.Value(1)).current;
-  const [hideUI, setHideUI] = useState(false);
+  const {hideUI, setHideUI} = useLayoutContext();
+  const [viewDisplay, setViewDisplay] = useState('flex');
 
-  //TODO implements fade UI
-  const fadeUI = (toValue: number) => {
-    if (toValue === 1) setHideUI(false);
+  const uiTranslateY = useRef(new Animated.Value(0)).current;
+  const currentOffset = useRef(0);
 
-    Animated.timing(uiOpacity, {
-      toValue,
-      duration: 300,
-      useNativeDriver: true,
-    }).start(() => {
-      if (toValue === 0) setHideUI(true);
-    });
+  const ANIMATION_DURATION = 200;
+  const handleScroll = (event: any) => {
+    const offsetY = event.nativeEvent.contentOffset.y;
+    if (offsetY > currentOffset.current + 10) {
+      Animated.timing(uiTranslateY, {
+        toValue: -350, 
+        duration: ANIMATION_DURATION,
+        useNativeDriver: true,
+      }).start();
+      setTimeout(()=> {
+        setViewDisplay('none')
+      },ANIMATION_DURATION);
+    }
+
+    if (offsetY < currentOffset.current - 10) {
+      Animated.timing(uiTranslateY, {
+        toValue: 0, 
+        duration: ANIMATION_DURATION,
+        useNativeDriver: true,
+      }).start();
+      setTimeout(()=> {
+        setViewDisplay('flex')
+      },ANIMATION_DURATION);
+    }
+
+    currentOffset.current = offsetY;
   };
 
 
   return (
     <TabsContainer>
-      <View className="px-6 pt-4">
-        <Header />
-
+      <Animated.View style={{ transform: [{ translateY: uiTranslateY }],}} className="px-6 pt-4">
         <Tabs value={currentScreen} onValueChange={(e) => setCurrentScreen(e as HomeScreen)}>
+        <Header />
           <TabsList className="flex-row w-full my-2">
             <TabsTrigger value="for-you" className="flex-1">
               <Text className={cn("text-xl", currentScreen === "for-you" && "text-blue-500")}>
@@ -58,7 +76,7 @@ const Home = () => {
           placeholder="Busque uma postagem..."
           suffixIcon={<AntDesign name="search1" size={24} color="grey" />}
         />
-      </View>
+      </Animated.View>
 
       <View className="flex-1 px-6">
         {currentScreen === "for-you" && (
@@ -67,6 +85,7 @@ const Home = () => {
             renderItem={() => <PostCard />}
             keyExtractor={(_, i) => i.toString()}
             contentContainerStyle={{ gap: 8 }}
+            onScroll={handleScroll}
           />
         )}
 
--- components/tabs-container.tsx
+++ components/tabs-container.tsx
@@ -1,4 +1,5 @@
 
+
 import React from 'react'
 import PostButton from './post-button'
 import { cn } from '@/lib/utils'
