5ee004ac58d7e3cb829c74126e4244f91926f0c3
Leonardo-Mazzuca <leonardodesouzamazzuca8@gmail.com>
2025-05-02 21:30:01 -0300

    adding categories scroll to newsletter page

--- app/(tabs)/newsletter.tsx
+++ app/(tabs)/newsletter.tsx
@@ -1,13 +1,30 @@
 
 
-import { Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import TabsContainer from '@/components/tabs-container'
+import Header from '@/components/header'
+import { Input } from '@/components/Input'
+import { Animated } from 'react-native'
+import CategoriesScroll from '@/components/categories-scroll'
+
+
 
 const Newsletter = () => {
+
+  const categories = ['Para você', 'TI', 'RH', 'Corporativo', 'Interno'];
+  const [selected, setSelected] = useState(categories[0]);
+
   return (
     <TabsContainer>
-      <Text>Newsletter</Text>
+      <Header/> 
+      <Animated.View className="px-6 pt-4">
+        <CategoriesScroll 
+          categories={categories}
+          selected={selected}
+          setSelected={setSelected} 
+        />
+        <Input />
+      </Animated.View>
     </TabsContainer>
   )
 }
---
+++ components/categories-scroll.tsx
@@ -0,0 +1,31 @@
+
+import { View, Text, FlatList, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { cn } from '@/lib/utils'
+
+
+type CategoriesScrollProps = {
+    categories: string[]
+    selected: string
+    setSelected: (selected: string) => void
+}
+
+const CategoriesScroll = ({categories,selected, setSelected}: CategoriesScrollProps) => {
+
+  
+  return (
+    <FlatList
+      data={categories}
+      renderItem={({item}) => (
+        <TouchableOpacity className={cn(selected === item ? 'bg-blue-primary' : 'bg-gray-300 ' ,'px-4 my-2 py-2 rounded-3xl mr-2')} onPress={() => setSelected(item)}>
+            <Text className={cn(selected === item ? 'text-white' : 'text-zinc-500' , 'font-semibold')}>
+                {item}
+            </Text>
+        </TouchableOpacity>
+      )}
+      horizontal
+    />
+  )
+}
+
+export default CategoriesScroll
