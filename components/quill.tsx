37922c23f97c06fc5b9f2775ff4bcd0cb6be989b
Leonardo Mazzuca <leonardo.mazzuca@umanni.com.br>
2025-05-08 14:00:00 -0300

    removing quill

--- components/quill.tsx
+++ components/quill.tsx
@@ -1,11 +1,4 @@
-import React, { useEffect } from "react";
-import {
-  KeyboardAvoidingView,
-  Platform,
-  View,
-} from "react-native";
-import { CodeBridge, RichText, Toolbar, useEditorBridge, useEditorContent } from "@10play/tentap-editor";
-import { SafeAreaView } from "react-native-safe-area-context";
+import React from "react";
 
 type QuillProps = {
   content: string,
@@ -15,37 +8,10 @@ type QuillProps = {
 
 const Quill = ({content,setContent,setError}:QuillProps) => {
 
-  const editor = useEditorBridge({
-    autofocus: true,
-    avoidIosKeyboard: true,
-    initialContent: content,
-  });
   
-  const editorContent = useEditorContent(editor, { type: 'html' });
-  const textContent = useEditorContent(editor, { type: 'text' });
-
-  useEffect(() => {
-    editorContent && setContent(editorContent);
-    console.log(textContent?.length);
-    if ( textContent?.length === 0 ) {
-      setError();
-    }
-  }, [editorContent,textContent]);
   
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <RichText editor={editor} />
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        style={{
-          position: "absolute",
-          width: "100%",
-          bottom: 0,
-        }}
-      >
-        <Toolbar editor={editor} />
-      </KeyboardAvoidingView>
-    </SafeAreaView>
+    <></>
   );
 };
 
