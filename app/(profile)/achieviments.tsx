b3b508bab42fbc7d49d669c4a6df7716a8903e24
Leonardo-Mazzuca <leonardodesouzamazzuca8@gmail.com>
2025-05-22 21:15:25 -0300

    adding points to achieviments

--- app/(profile)/achieviments.tsx
+++ app/(profile)/achieviments.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import ProfileContainer from '@/components/profile-container'
 import ProfileHeader from '@/components/profile-header'
-import { FlatList, View } from 'react-native'
+import { FlatList, Text, View } from 'react-native'
 import { achieviments } from '@/constants/data'
 import AchievimentCard from '@/components/achieviment-card'
 
@@ -13,10 +13,21 @@ const Achieviments = () => {
       <ProfileHeader
         text="Minhas conquistas"
       />
-
       <View className='px-6 flex-1'>
+
+      <View className='flex-row gap-1 my-3 items-center'>
+        <Text className='dark:text-white text-xl font-semibold'>
+          Meus pontos
+        </Text>
+        <View className='bg-green-500/25 rounded-xl ms-2 px-2'>
+          <Text className='text-green-500 text-xl font-semibold'>
+            1900
+          </Text>
+        </View>
+      </View>
+
         <FlatList 
-          className='mt-10 border-2 dark:border-zinc-800 border-gray-300 rounded-lg'
+          className='mt-3 border-2 dark:border-zinc-800 border-gray-300 rounded-lg'
           data={achieviments}
           keyExtractor={(_, index) => index.toString()}
           renderItem={({item})=> (
